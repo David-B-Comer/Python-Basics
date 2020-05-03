df1d90f2feb8115051c1343b664a5b5b18982a01
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-05-03 16:39:44 -0400

    math

---
+++ python_excercises_1/math.py
@@ -0,0 +1,22 @@
+# Using the Python interpreter, perform the following operations. Be sure to experiment with mixing intergers and floats.
+
+# Addition
+2+2
+
+# Subtraction
+3.5-2.5
+
+# Multiplication
+4*4
+
+# Division
+16/4
+
+#Floor division
+15.5//5
+
+# Modulo
+8%3
+
+# Exponentiation
+5**2
