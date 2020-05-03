1341e69267a5f29fe5c656c390be62852c017b1f
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-05-03 16:31:28 -0400

    types

---
+++ python_excercises_1/types.py
@@ -0,0 +1,19 @@
+# Using the Python interpreter, identify the data type for the following values:
+
+# True
+bool
+
+# 42
+int
+
+# False
+bool
+
+# 1.5
+float
+
+# "Python is fun"
+str
+
+# "3.5"
+str
