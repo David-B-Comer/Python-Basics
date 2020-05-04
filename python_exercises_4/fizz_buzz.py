8b1771442d2f6d7625087d51cd1eb64229f5c373
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-05-04 12:19:58 -0400

    fizzbuzz

--- python_exercises_4/fizz_buzz.py
+++ python_exercises_4/fizz_buzz.py
@@ -5,3 +5,20 @@ The program should meet the following criteria:
 	If the number is a multiple of 5, print "Buzz" instead of the number.
 	If the number is a multiple of both 3 and 5, print "FizzBuzz" instead of the number.
 """
+
+
+def main():
+	for fizzbuzz in range(101):
+		if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+			print("fizzbuzz")
+			continue
+		elif fizzbuzz % 3 == 0:
+			print("fizz")
+			continue
+		elif fizzbuzz % 5 == 0:
+			print("buzz")
+			continue
+		print(fizzbuzz)
+
+if __name__ == '__main__':
+    main()
