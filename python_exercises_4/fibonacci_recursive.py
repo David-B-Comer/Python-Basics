4a7c7dd1f63fc4d50398187f52fafc56a69bca80
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-05-05 15:19:19 -0400

    fib-recursive

---
+++ python_exercises_4/fibonacci_recursive.py
@@ -0,0 +1,26 @@
+"""
+Given a term (n), determine the value of x(n).
+In the fibonacci_recursive.py program, create a function called fibonacci. The function should take in an integer and return the value of x(n).
+This problem must be solved using recursion.
+"""
+
+import sys
+
+def fibonacci(n):
+	if n < 0:
+		print("Incorrect input")
+	elif n == 1:
+		return 0
+	elif n == 2:
+		return 1
+	else:
+		return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def main():
+    print('Enter a number : ')
+    x = int(sys.stdin.readline())
+    print(fibonacci(x))
+
+if __name__=='__main__':
+    main()
